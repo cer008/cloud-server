a21cce0165103c257b93ba7ad4905ae1f94d4465
472829302@qq.com <xj20170721>
2020-05-10 20:48:07 +0800

    add

---
+++ eureka-core/src/main/java/com/example/core/eurekacore/EurekaCoreApplication.java
@@ -0,0 +1,15 @@
+package com.example.core.eurekacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaCoreApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EurekaCoreApplication.class, args);
+    }
+
+}
